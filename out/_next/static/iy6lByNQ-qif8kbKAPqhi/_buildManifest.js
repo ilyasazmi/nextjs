907d1f85da853314bc49b0af4177be9099092f63
ilyas azmi <ilyasazmi@live.com>
2023-08-03 17:04:22 +0800

    out as published page

---
+++ out/_next/static/iy6lByNQ-qif8kbKAPqhi/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/356-747a496486dfe077.js","static/chunks/pages/index-ae4e53547cae8ce4.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
